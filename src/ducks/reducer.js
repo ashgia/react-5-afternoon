6bfa5b57b88e694cbce0e2f9e913e9691e29c1c9
Ashley Nguyen <ashleygnguyen@yahoo.com>
2018-08-27 21:55:32 -0500

    edit

--- src/ducks/reducer.js
+++ src/ducks/reducer.js
@@ -19,14 +19,28 @@ const initialState = {
 
 const UPDATE_LOAN_TYPE = "UPDATE_LOAN_TYPE";
 const UPDATE_PROPERTY_TYPE = "UPDATE_PROPERTY_TYPE";
+const UPDATE_CITY = "UPDATE_CITY";
+const UPDATE_PROP = "UPDATE_PROP";
+const UPDATE_FOUND = "UPDATE_FOUND";
 
 function reducer(state = initialState, action) {
+  console.log("REDUCER HIT: Action ->", action);
+
   switch (action.type) {
     case UPDATE_LOAN_TYPE:
       return Object.assign({}, state, { loanType: action.payload });
 
     case UPDATE_PROPERTY_TYPE:
       return Object.assign({}, state, { propertyType: action.payload });
+
+    case UPDATE_CITY:
+      return Object.assign({}, state, { city: action.payload });
+
+    case UPDATE_PROP:
+      return Object.assign({}, state, { propToBeUsedOn: action.payload });
+
+    case UPDATE_FOUND:
+      return Object.assign({}, state, { found: action.payload });
 
     default:
       return state;
@@ -44,6 +58,27 @@ export function updatePropertyType(property) {
   return {
     type: UPDATE_PROPERTY_TYPE,
     payload: property
+  };
+}
+
+export function updateCity(city) {
+  return {
+    type: UPDATE_CITY,
+    payload: city
+  };
+}
+
+export function updateProp(prop) {
+  return {
+    type: UPDATE_PROP,
+    payload: prop
+  };
+}
+
+export function updateFound(found) {
+  return {
+    type: UPDATE_FOUND,
+    payload: found
   };
 }
 
